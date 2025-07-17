b95b07af2a90b6ac608746710e13567105546495
Aman Pal <aman.pal@tothenew.com>
2025-07-17 16:50:20 +0530

    updated for constructor injection

--- src/main/java/org/example/Q3_Q4_Q5_Q6/BinarySearch.java
+++ src/main/java/org/example/Q3_Q4_Q5_Q6/BinarySearch.java
@@ -1,17 +1,20 @@
 package org.example.Q3_Q4_Q5_Q6;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearch {
 
-    @Autowired
+
     Sorting sorting;
 
-
+    @Autowired
     public BinarySearch(Sorting sorting) {
         this.sorting = sorting;
     }
